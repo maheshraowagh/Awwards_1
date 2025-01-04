64b3eeb2d19b1974e2df642ca0922b0cad7fe6ab
Mahesh <maheshwagh079@gmail.com>
2025-01-04 14:09:29 +0530

    final work

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Header from './Sections/Header'
 import Hero from './Sections/Hero'
+import Features from './Sections/Features'
+import Pricing from './Sections/Pricing'
+import Faq from './Sections/Faq'
+import Testimonials from './Sections/Testimonials'
+import Download from './Sections/Download'
+import Footer from './Sections/Footer'
 
 const App = () => {
   return (
     <main className='overflow-hidden'>
       <Header/>
       <Hero/>
+      <Features />
+      <Pricing/>
+      <Faq/>
+      <Testimonials/>
+      <Download/>
+      <Footer/>
+
   </main>
   )
 }
--- Website/src/Sections/Header.jsx
+++ Website/src/Sections/Header.jsx
@@ -1,106 +1,117 @@
-import React, { useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 
-const NavLink = ({ title, to }) => (
-  <LinkScroll
-    to={to} // Specify the section ID to scroll to
-    smooth={true}
-    duration={500}
-    offset={-70} // Adjust scroll offset if needed (e.g., for fixed headers)
-    className="base-bold text-p4 uppercase transition-colors duration-500 
-      cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
-  >
-    {title}
-  </LinkScroll>
-);
-
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
-        {/* Logo */}
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src="/images/xora.svg" width={115} height={55} alt="Xora Logo" />
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
-        {/* Navigation */}
         <div
           className={clsx(
             "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
-            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none "
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",
           )}
-          //     className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2
-          //       max-lg:transition-all max-lg:duration-300 ${
-          //         isOpen ? "max-lg:opacity-100 max-lg:translate-x-0" : "max-lg:opacity-0 max-lg:-translate-x-full"
-          //       }`}
         >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
-            <nav className="max-lg:mt-32">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
-                  <NavLink title="Features" to="features" />
+                  <NavLink title="features" />
                   <div className="dot" />
-                  <NavLink title="Pricing" to="pricing" />
+                  <NavLink title="pricing" />
                 </li>
 
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
-                      "max-lg:hidden transition-transform duration-500 cursor-pointer"
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer",
                     )}
                   >
                     <img
                       src="/images/xora.svg"
                       width={160}
                       height={55}
-                      alt="Xora Logo"
+                      alt="logo"
                     />
                   </LinkScroll>
                 </li>
 
                 <li className="nav-li">
-                  <NavLink title="FAQ" to="faq" />
+                  <NavLink title="faq" />
                   <div className="dot" />
-                  <NavLink title="Download" to="download" />
+                  <NavLink title="download" />
                 </li>
               </ul>
             </nav>
-            <div className="lg:hidden block absolute top-[20%] left-0 w-[960px] h-[0px] translate-x-[-290px] -translate-y-1/2">
-  <img
-    src="/images/bg-outlines.svg"
-    width={960}
-    height={380}
-    alt="outline"
-    className="relative z-2 rotate-90 "
-  />
-  <img
-    src="/images/bg-outlines-fill.png"
-    width={960}
-    height={380}
-    alt="outline"
-    className="absolute inset-0 mix-blend-soft-light opacity-5 rotate-90"
-  />
-</div>
 
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
           </div>
         </div>
 
-        {/* Mobile Menu Toggle Button */}
         <button
-          className="lg:hidden z-2 size-10 border-2 border-s4/25 
-            rounded-full flex justify-center items-center"
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
           onClick={() => setIsOpen((prevState) => !prevState)}
         >
           <img
             src={`/images/${isOpen ? "close" : "magic"}.svg`}
-            alt="Menu Toggle"
+            alt="magic"
             className="size-1/2 object-contain"
           />
         </button>
